2523006f7d835f79e4ad4c37fab5235ad2006bd2
r0i <r0i@darksideteam.com>
2016-05-28 20:46:10 +0200

    Inicio Next-OS

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -42,11 +42,11 @@ except ImportError:
 DEBUG = False
 default_manifest = ".repo/manifest.xml"
 
-custom_local_manifest = ".repo/local_manifests/slim_manifest.xml"
-custom_default_revision = "mm6.0"
-custom_dependencies = "slim.dependencies"
-org_manifest = "SlimRoms"  # leave empty if org is provided in manifest
-org_display = "SlimRoms"  # needed for displaying
+custom_local_manifest = ".repo/local_manifests/next_manifest.xml"
+custom_default_revision = "mm"
+custom_dependencies = "next.dependencies"
+org_manifest = "Next-OS"  # leave empty if org is provided in manifest
+org_display = "Next-OS"  # needed for displaying
 
 github_auth = None
 
